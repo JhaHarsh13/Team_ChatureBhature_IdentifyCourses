60e15107c46a1ce435d048e0abf4c7a13ed16638
Harsh Kumar Jha <harshkumarjha1331@gmail.com>
2025-07-14 17:44:59 +0530

    File added to git repo

---
+++ IdentifyCourses.Coursera.org/src/test/java/UtilityFile/ExcelUtils.java
@@ -0,0 +1,38 @@
+package UtilityFile;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Set;
+
+public class ExcelUtils {
+	public static void writingToExcel(Set <String> set) throws Exception{
+		FileInputStream file=new FileInputStream("C:\\Users\\2408293\\git\\Team_ChatureBhature_IdentifyCourses\\IdentifyCourses.Coursera.org\\src\\test\\resources\\Count_Of_Languages.xlsx");
+		Workbook workBook=new XSSFWorkbook(file);
+		file.close();
+		Sheet sheet = workBook.getNumberOfSheets()==0 ? workBook.createSheet("Languages Count") : workBook.getSheetAt(0);
+		if(sheet.getPhysicalNumberOfRows()==0) {
+			Row header=sheet.createRow(0);
+			Cell headerCell= header.createCell(0);
+			headerCell.setCellValue("Languages And Count");
+		}
+		
+		int rowNum = sheet.getLastRowNum() + 1;
+		for(String language: set) {
+			Row row = sheet.createRow(rowNum);
+			row.createCell(0).setCellValue(language);
+			rowNum++;
+		}
+		
+		FileOutputStream outPutFile=new FileOutputStream("C:\\Users\\2408293\\git\\Team_ChatureBhature_IdentifyCourses\\IdentifyCourses.Coursera.org\\src\\test\\resources\\Count_Of_Languages.xlsx");
+		workBook.write(outPutFile);
+		workBook.close();
+		
+		
+	}
+}
