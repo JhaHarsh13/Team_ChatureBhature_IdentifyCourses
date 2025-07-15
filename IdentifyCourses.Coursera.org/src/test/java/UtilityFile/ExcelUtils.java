c9c65f8c680885ccc44e929579895ef1c471d10c
Harsh Kumar Jha <harshkumarjha1331@gmail.com>
2025-07-16 00:31:55 +0530

    Recommited as no changes happened from the previous commit and minor refactorings to the code was done

--- IdentifyCourses.Coursera.org/src/test/java/UtilityFile/ExcelUtils.java
+++ IdentifyCourses.Coursera.org/src/test/java/UtilityFile/ExcelUtils.java
@@ -12,7 +12,7 @@ import java.util.Set;
 
 public class ExcelUtils {
 	public static void writingToExcel(Set <String> set) throws Exception{
-		FileInputStream file=new FileInputStream(System.getProperty("user.dir")+"\\resources\\Count_Of_Languages.xlsx");
+		FileInputStream file=new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\Count_Of_Languages.xlsx");
 		Workbook workBook=new XSSFWorkbook(file);
 		file.close();
 		Sheet sheet = workBook.getNumberOfSheets()==0 ? workBook.createSheet("Languages Count") : workBook.getSheetAt(0);
@@ -32,7 +32,6 @@ public class ExcelUtils {
 		FileOutputStream outPutFile=new FileOutputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\Count_Of_Languages.xlsx");
 		workBook.write(outPutFile);
 		workBook.close();
-		
 		
 	}
 }
