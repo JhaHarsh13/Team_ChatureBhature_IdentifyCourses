3131de170ae0e86836877bf2a0342dcc79543dba
Harsh Kumar Jha <harshkumarjha1331@gmail.com>
2025-07-16 10:24:50 +0530

    Conflict resolved again

--- IdentifyCourses.Coursera.org/src/test/java/ConfigureReader/ConfigReader.java
+++ IdentifyCourses.Coursera.org/src/test/java/ConfigureReader/ConfigReader.java
@@ -8,13 +8,7 @@ public class ConfigReader {
 	private static Properties property;
 	static {
 		try {
-<<<<<<< HEAD
 			FileInputStream file= new FileInputStream(System.getProperty("user.dir")+"\\data.properties");
-=======
-
-			FileInputStream file= new FileInputStream(System.getProperty("user.dir")+"\\data.properties");
-
->>>>>>> 6dc509306c7a3bf494a42661937b4385ba3f4f25
 			property=new Properties();
 			property.load(file);
 		}catch(Exception ex) {
